5256372ad434bbd16e74cea75e091fa799b55740
Michael Yenny <amichaelj1@gmail.com>
2019-07-10 22:24:26 -0400

    Added Sorting

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,37 +4,39 @@ const mongodb = require('mongodb')
 const router = express.Router()
 
 // Add post
-router.post('/', async (req, res) => {
-  const posts = await loadPostsCollection()
-  await posts.insertOne({
-    text: req.body.text,
-    createdAt: new Date()
-  })
-  res.status(201).send()
-})
-// Get post
-router.get('/', async (req, res) => {
-  const posts = await loadPostsCollection()
-  res.send(await posts.find({}).toArray())
+router.post('/', async(req, res) => {
+        const posts = await loadPostsCollection()
+        await posts.insertOne({
+            text: req.body.text,
+            createdAt: new Date()
+        })
+        res.status(201).send()
+    })
+    // Get post
+router.get('/', async(req, res) => {
+    const posts = await loadPostsCollection()
+    res.send(await posts.find({}).sort({
+        "createdAt": -1
+    }).toArray())
+
 })
 
 // Delete post
-router.delete('/:id', async (req, res) => {
-  const posts = await loadPostsCollection()
-  await posts.deleteOne({
-    _id: new mongodb.ObjectID(req.params.id)
-  })
-  res.status(200).send()
+router.delete('/:id', async(req, res) => {
+    const posts = await loadPostsCollection()
+    await posts.deleteOne({
+        _id: new mongodb.ObjectID(req.params.id)
+    })
+    res.status(200).send()
 })
 
 async function loadPostsCollection() {
-  const client = await mongodb.MongoClient.connect(
-    'mongodb+srv://abc123:michael123@cluster0-yavih.mongodb.net/test?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true
-    }
-  )
-  return client.db('vuexpress').collection('posts')
+    const client = await mongodb.MongoClient.connect(
+        'mongodb+srv://abc123:michael123@cluster0-yavih.mongodb.net/test?retryWrites=true&w=majority', {
+            useNewUrlParser: true
+        }
+    )
+    return client.db('vuexpress').collection('posts')
 }
 
 module.exports = router
